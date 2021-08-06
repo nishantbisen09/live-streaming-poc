c62bb54feb08c66a94f0c6a1959b661c6160074e
Your Name <you@example.com>
2021-08-06 19:48:58 +0530

    implement video live straming

--- client/src/components/VideoParticipant.js
+++ client/src/components/VideoParticipant.js
@@ -1,12 +1,5 @@
-const VideoParticipant = ({ name, picture, stream }) => {
-  return (
-    <div className="video-participant">
-      <a href="/" className="name-tag">
-        {name}
-      </a>
-      <img src={picture} alt="participant" />
-    </div>
-  );
+const VideoParticipant = ({ name, picture }) => {
+  return <video className="video-participant" src="" />;
 };
 
 export default VideoParticipant;
--- client/src/pages/CallPage.js
+++ client/src/pages/CallPage.js
@@ -1,12 +1,102 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Peer from "simple-peer";
+import io from "socket.io-client";
 import ChatBar from "../components/ChatBar";
 import VideoCallActions from "../components/VideoCallActions";
 import VideoParticipant from "../components/VideoParticipant";
+import { BASE_URL, GET_CALL_ID, SAVE_CALL_ID } from "../utils/apiEndpoints";
+import { getRequest, postRequest } from "../utils/apiRequests";
+
+let peer = null;
+let socket = io.connect(`${window.location.hostname}:4000`);
+// const initialState = [];
 
 const CallPage = () => {
   const { id } = useParams();
   const isHost = window.location.hash === "#init";
   const url = `${window.location.origin}${window.location.pathname}`;
+
+  // const [streamObj, setStreamObj] = useState();
+  // const [screenCastStream, setScreenCastStream] = useState();
+  // const [isPresenting, setIsPresenting] = useState(false);
+  // const [isMessenger, setIsMessenger] = useState(false);
+  // const [messageAlert, setMessageAlert] = useState({});
+  // const [isAudio, setIsAudio] = useState(true);
+
+  // const [messageList, messageListReducer] = useReducer(
+  //   MessageReducer,
+  //   initialState
+  // );
+
+  useEffect(() => {
+    initWebRTC();
+    socket.on("code", data => {
+      if (data.url === url) {
+        peer.signal(data.code);
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getReceiverCode = async () => {
+    const response = await getRequest(`${BASE_URL}${GET_CALL_ID}/${id}`);
+    if (response.code) {
+      peer.signal(response.code);
+    }
+  };
+
+  const initWebRTC = () => {
+    console.log(navigator.mediaDevices);
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then(stream => {
+        peer = new Peer({
+          initiator: isHost,
+          trickle: false,
+          stream: stream,
+        });
+
+        if (!isHost) {
+          getReceiverCode();
+        }
+
+        peer.on("signal", async data => {
+          if (isHost) {
+            const payload = {
+              id,
+              signalData: data,
+            };
+
+            await postRequest(`${BASE_URL}${SAVE_CALL_ID}`, payload);
+          } else {
+            socket.emit("code", { code: data, url }, cb => {
+              console.log("code sent");
+            });
+          }
+        });
+
+        peer.on("connect", () => {
+          console.log("peer connected");
+        });
+
+        peer.on("stream", stream => {
+          let video = document.querySelector("video");
+
+          if ("srcObject" in video) {
+            video.srcObject = stream;
+            console.log(stream);
+          } else {
+            video.src = window.URL.createObjectURL(stream); // for older browsers
+          }
+
+          video.play();
+        });
+      });
+  };
 
   return (
     <div className="app-container">
@@ -16,12 +106,6 @@ const CallPage = () => {
             name="Andy Will"
             picture={
               "https://images.unsplash.com/photo-1566821582776-92b13ab46bb4?ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60"
-            }
-          />
-          <VideoParticipant
-            name="Emmy Lou"
-            picture={
-              "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
             }
           />
         </div>
